fbe28ed508ce83154a87fc7577f3cb37234fa3e5
Maximizer02 <146656033+Maximizer02@users.noreply.github.com>
2025-09-10 00:37:11 +0200

    restructure and rebrand

---
+++ Downloader.cs
@@ -0,0 +1,111 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace BetterYoutubeDownloader
+{
+    public class Downloader
+    {
+        private YesOrNo menu = new YesOrNo();
+        private Editor editor = new Editor();
+        public void downloadVideo()
+        {
+            do
+            {
+                string youtubeFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Videos\YoutubeDownloads";
+                string[] alreadyDownloadedMedia = Directory.GetFiles(youtubeFolder);
+                ytdlp(getValidLink(), "-f mp4 --restrict-filenames -P ~/Videos/YoutubeDownloads ");
+                sanitizeFilenameAndEdit(youtubeFolder, alreadyDownloadedMedia, false);
+
+                Console.WriteLine("Download another?");
+            } while (menu.displayMenu() == 0);
+            Console.WriteLine();
+        }
+
+        public void downloadAudio()
+        {
+            do
+            {
+                string youtubeFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Music\YoutubeDownloads";
+                string[] alreadyDownloadedMedia = Directory.GetFiles(youtubeFolder);
+                ytdlp(getValidLink(), "-x --audio-format mp3 --restrict-filenames --embed-thumbnail --embed-metadata -P ~/Music/YoutubeDownloads ");
+                sanitizeFilenameAndEdit(youtubeFolder, alreadyDownloadedMedia,true);
+                Console.WriteLine("Download another?");
+            } while (menu.displayMenu() == 0);
+            Console.WriteLine();
+        }
+
+        private void ytdlp(string link, string arguments)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "yt-dlp";
+            process.StartInfo.Arguments = arguments + " " + link;
+            try
+            {
+                process.Start();
+                process.WaitForExit();
+                Console.ForegroundColor = process.ExitCode == 1 ? ConsoleColor.Red : ConsoleColor.Green;
+                Console.WriteLine("Exit Code: " + process.ExitCode);
+                Console.ResetColor();
+            }
+            catch
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Could not access yt-dlp");
+                Console.WriteLine("Be sure to have it installed and added to $PATH");
+                Console.WriteLine("For more info look at the about section");
+            }
+            Console.WriteLine();
+        }
+
+
+        private string getValidLink()
+        {
+            Console.WriteLine("Enter Youtube URL here:");
+            string link = Console.ReadLine();
+            if (validateLink(link))
+            {
+                return link;
+            }
+            else
+            {
+                Console.WriteLine("Link invalid, try again");
+                return getValidLink();
+            }
+        }
+
+        private bool validateLink(string link)
+        {
+            Uri uriResult;
+            return Uri.TryCreate(link, UriKind.Absolute, out uriResult)
+                && uriResult.Scheme == Uri.UriSchemeHttps;
+        }
+
+        private void sanitizeFilenameAndEdit(string youtubeFolder, string[] alreadyDownloadedMedia,bool edit) 
+        {
+            try
+            {
+                string[] alreadyDownloadedMediaIncludingNew = Directory.GetFiles(youtubeFolder);
+                string newFileName = alreadyDownloadedMediaIncludingNew.Where(x => !alreadyDownloadedMedia.Contains(x)).First();
+                string newFileNameSanitized = Regex.Replace(newFileName, @"\[(.*?)\]", "");
+                File.Move(newFileName, newFileNameSanitized);
+                if (edit) 
+                { 
+                    Console.WriteLine("Edit Metadata?");
+                    if (menu.displayMenu() == 0) editor.editMetadataFromPath(newFileNameSanitized);
+                }
+            }
+            catch (IOException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Cannot sanitize file name, file already exists!");
+                Console.ResetColor();
+            }
+            catch (InvalidOperationException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("File was already downloaded");
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ Editor.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Id3;
+using Id3.Frames;
+
+namespace BetterYoutubeDownloader
+{
+    public class Editor
+    {
+        YesOrNo editMoreDatapoints = new YesOrNo();
+        
+        public void editMetadataFromConsoleInput()
+        {
+
+            Console.WriteLine("Enter file name or path:");
+            string filePath = Console.ReadLine();
+            editMetadata(filePath);
+    
+        }
+        //Want to make it so that when you download a file you can edit the metadata directly by passing the file path into this method.
+        //But idk how to get the path from yt-dlp
+        public void editMetadataFromPath(string path)
+        {
+
+            Console.WriteLine("Enter file name or path:");
+            editMetadata(path);
+
+        }
+
+
+        private void editMetadata(string filePath) 
+        {
+            Mp3 file;
+            try
+            {
+
+                file = accessFile(filePath);
+
+                //Idunno if IOException is really corrent here but I didn't know what else to pick as there is no specific Exception for this Situation lol
+                if (!file.HasTags) { file.Dispose(); throw new IOException("The selected file has no Tags!"); }
+                Id3Tag tag = file.GetTag(Id3TagFamily.Version2X);
+
+                List<string> EditMetadataMenuOptions = new List<string>()
+                    {
+                     "Title: "+(!tag.Title.IsAssigned?"N/A":tag.Title),
+                     "Artist: "+(!tag.Artists.IsAssigned?"N/A":tag.Artists),
+                     "Album: "+(!tag.Album.IsAssigned?"N/A":tag.Album),
+                     "Year: "+(!tag.Year.IsAssigned?"N/A":tag.Year),
+                     "Genre: "+(!tag.Genre.IsAssigned?"N/A":tag.Genre),
+                     "Nothing really"
+                    };
+                Menu editMetadataMenu = new Menu(EditMetadataMenuOptions);
+
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nFile found.");
+                Console.ResetColor();
+                Console.Write(" What do you want to edit?\n");
+                do
+                {
+                    switch (editMetadataMenu.displayMenu())
+                    {
+                        case 0: Console.WriteLine("Please enter new Title:"); tag.Title = Console.ReadLine(); break;
+                        case 1:
+                            Console.WriteLine("Please enter new Artist name:");
+                            string newArtist = Console.ReadLine();
+                            string[] newArtists = newArtist!.Split("||");
+                            tag.Artists.Value.Clear();
+                            foreach (string artist in newArtists)
+                            tag.Artists.Value.Add(artist);
+
+                        
+                            break;
+                        case 2: Console.WriteLine("Please enter new Album name:"); tag.Album = Console.ReadLine(); break;
+                        case 3: Console.WriteLine("Please enter new Year:"); tag.Year.Value = int.Parse(Console.ReadLine()); break;
+                        case 4: Console.WriteLine("Please enter new Genre:"); tag.Genre = Console.ReadLine(); break;
+                    }
+                    Console.WriteLine("\nEdit another datapoint?");
+                }
+                while (editMoreDatapoints.displayMenu() == 0);
+
+
+                file.WriteTag(tag);
+                file.Dispose();
+                Console.WriteLine();
+
+            }
+            catch (FormatException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please enter a correct number!\n");
+                Console.ResetColor();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message + "\n");
+                Console.ResetColor();
+            }
+        }
+
+        private Mp3 accessFile(string file)
+        {
+            string current = Directory.GetCurrentDirectory();
+
+            //check if 'file' is valid path
+            if (File.Exists(file)) return new Mp3(file, Mp3Permissions.ReadWrite);
+
+            //check if 'file' is a valid file in current directory
+            if (File.Exists(current + @"\"+file)) return new Mp3(current + @"\" + file, Mp3Permissions.ReadWrite);
+
+            //check if 'file' is the name(without extension) of a valid file in current directory
+            List<string> possibleFiles = Directory.GetFiles(current, "*.mp3")
+                .Where(f => f.Substring(current.Length + 1).Split('.')[0].Equals(file))
+                .ToList();
+            if (possibleFiles.Count > 0)  return new Mp3(possibleFiles.First(), Mp3Permissions.ReadWrite); 
+
+            throw new FileNotFoundException("This file does not exist!");
+        }
+       
+    }
+}
